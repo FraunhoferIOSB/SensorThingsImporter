073c8fa854d4f7933b62db930a2048ae41757e33
Hylke van der Schaaf <hylke.vds@gmail.com>
2021-05-22 21:11:30 +0200

    Set timeouts for connections

--- src/main/java/de/fraunhofer/iosb/ilt/sensorthingsimporter/utils/UrlUtils.java
+++ src/main/java/de/fraunhofer/iosb/ilt/sensorthingsimporter/utils/UrlUtils.java
@@ -31,11 +31,13 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.ParseException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
@@ -77,7 +79,15 @@ public class UrlUtils {
 	}
 
 	private static String readNormalUrl(String targetUrl, Charset charset) throws IOException, ParseException {
-		try (CloseableHttpClient client = HttpClients.createSystem()) {
+		HttpClientBuilder clientBuilder = HttpClientBuilder.create()
+				.useSystemProperties()
+				.setDefaultRequestConfig(
+						RequestConfig.custom()
+								.setSocketTimeout(20_000)
+								.setConnectTimeout(10_000)
+								.setConnectionRequestTimeout(10_000)
+								.build());
+		try (CloseableHttpClient client = clientBuilder.build()) {
 			HttpGet get = new HttpGet(targetUrl);
 			CloseableHttpResponse response = client.execute(get);
 			final int statusCode = response.getStatusLine().getStatusCode();
