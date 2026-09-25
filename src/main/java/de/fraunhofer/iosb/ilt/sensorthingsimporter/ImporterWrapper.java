c57f32039e090527d2bf37c601e4d18ce8584ed0
Hylke van der Schaaf <hylke.vds@gmail.com>
2026-09-25 10:44:36 +0200

    Split multicatch

--- src/main/java/de/fraunhofer/iosb/ilt/sensorthingsimporter/ImporterWrapper.java
+++ src/main/java/de/fraunhofer/iosb/ilt/sensorthingsimporter/ImporterWrapper.java
@@ -343,7 +343,10 @@ public class ImporterWrapper implements AnnotatedConfigurable<SensorThingsServic
                 LOGGER.error("Code: {} {}", exc.getStatusCode(), exc.getStatusMessage());
                 LOGGER.error("Data: {}", exc.getReturnedContent());
                 LOGGER.debug("Failed to upload.", exc);
-            } catch (ServiceFailureException | RuntimeException exc) {
+            } catch (RuntimeException exc) {
+                LOGGER.error("Failed to serialise: {}", exc.getMessage());
+                LOGGER.debug("Details:", exc);
+            } catch (ServiceFailureException exc) {
                 LOGGER.error("Failed to upload: {}", exc.getMessage());
                 LOGGER.debug("Details:", exc);
             }
